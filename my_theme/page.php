cf1512eedc2052e85fa2e31809181a48e3df87be
changelate <122318537+changelate@users.noreply.github.com>
2025-10-08 18:26:39 +0300

    Add files via upload

---
+++ my_theme/page.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package my_theme
+ */
+
+get_header();
+?>
+
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package my_theme
+ */
+
+get_header();
+?>
+
+<?php if ( have_posts() ) : ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+		<?php the_title( '<h2>', '</h2>' ); ?>
+	<?php endwhile; ?>
+<?php else : ?>
+	<p>Постов нет</p>
+<?php endif; ?>
+
+<?php
+get_footer();
+?>
+
+<?php
+get_footer();
+?>
